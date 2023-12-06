cb5dbc37180bb4a55348de68607c0b94c6f2a6c9
Priya1w21 <aarepriyanka17@gmail.com>
2023-12-06 12:32:51 +0530

    made listitems into bold and 3rd list to green

---
+++ demo.js
@@ -0,0 +1,5 @@
+document.getElementById("thirdlist").style.backgroundColor="green"
+var listItems=document.getElementsByClassName("list-group-item");
+for(var i=0;i<listItems.length;i++){
+    listItems[i].style.fontWeight="bold";
+}
